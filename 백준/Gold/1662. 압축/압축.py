4bf7c0a7eb0c22037950396460717ff4f6928ae3
JunWon11 <125115780+JunWon11@users.noreply.github.com>
2023-07-31 14:27:41 +0900

    [Gold V] Title: 압축, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/1662. 압축/압축.py
+++ 백준/Gold/1662. 압축/압축.py
@@ -1,26 +1,24 @@
-import sys
+s = input()
 
-s = sys.stdin.readline().strip()
 stack = []
 
 for i in range(len(s)):
-    if s[i] == "(":
+    if i < len(s) - 1 and s[i + 1] == '(':
+        stack.append(int(s[i]))
+    elif '0' <= s[i] <= '9':
+        stack.append(1)
+    elif s[i] == '(':
         stack.append(s[i])
-    elif s[i] == ")":
+    elif s[i] == ')':
         cnt = 0
         while 1:
-            tmp = stack.pop()
-            if tmp == "(":
-                break
-            cnt += tmp
+            k = stack.pop()
+            if k == '(':break
+            cnt += k
         stack.append(int(stack.pop()) * cnt)
-    elif i < len(s) - 1 and s[i + 1] == "(":
-        stack.append(int(s[i]))
-    else:
-        stack.append(1)
 
 result = 0
 
-for k in stack:
-    result += k
+for _ in stack:
+    result += _
 print(result)
