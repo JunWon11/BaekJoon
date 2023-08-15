9ef2b79c270482654116bbd0dfd69e9053ce61cf
JunWon11 <125115780+JunWon11@users.noreply.github.com>
2023-08-15 23:23:20 +0900

    [Silver II] Title: 이동하기, Time: 288 ms, Memory: 124716 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -5,12 +5,11 @@ n, m = map(int, input().split())
 miro = []
 for i in range(n):
     miro.append(list(map(int, input().split())))
-miro.insert(0, [0] * m)
+    miro[i].insert(0, 0)
+
+miro.insert(0, [0] * (m+1))
 for i in range(1, n+1):
-    for j in range(m):
-        if j > 0:
-            miro[i][j] += max(miro[i-1][j], miro[i][j-1], miro[i-1][j-1])
-        else:
-            miro[i][j] += miro[i-1][j] 
+    for j in range(1, m+1):
+        miro[i][j] += max(miro[i-1][j], miro[i][j-1], miro[i-1][j-1])
 
 print(max(miro[n]))
