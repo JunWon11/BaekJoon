c90a3421c96d9ec65b35eb7fc9142cdae0859926
JunWon11 <125115780+JunWon11@users.noreply.github.com>
2023-08-16 15:14:41 +0900

    [Gold V] Title: 동전 1, Time: 156 ms, Memory: 116140 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,8 +1,20 @@
-n,k=map(int,input().split())
-coins=[int(input()) for i in range(n)]
-dp=[0 for i in range(k+1)]
-dp[0]=1
-for coin in coins:
-    for i in range(coin,k+1):
-        if i-coin >=0:dp[i]+=dp[i-coin]
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = []
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for i in range(n):
+    coins.append(int(input()))
+
+coins.sort()
+
+for i in coins:
+    for j in range(1, k+1):
+        if j >= i:
+            dp[j] += dp[j-i]
+
 print(dp[k])
