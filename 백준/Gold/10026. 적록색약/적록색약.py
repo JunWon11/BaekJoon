722dd1b0f4477d4cc536b4ef7be6c5c7f20e6bf9
JunWon11 <125115780+JunWon11@users.noreply.github.com>
2023-08-25 21:30:38 +0900

    [Gold V] Title: 적록색약, Time: 264 ms, Memory: 119708 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -0,0 +1,65 @@
+from collections import deque
+from copy import deepcopy
+import sys
+input = sys.stdin.readline
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    while q:
+        v = q.popleft()
+        x, y = v
+        c = graph[x][y]
+        graph[x][y] = 0
+        visited[x][y] = 1
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == c and visited[nx][ny] == 0:
+                    q.append((nx, ny))
+    return 1
+
+def sbfs(x, y):
+    q = deque()
+    q.append((x, y))
+    while q:
+        v = q.popleft()
+        x, y = v
+        c = sgraph[x][y]
+        sgraph[x][y] = 0
+        svisited[x][y] = 1
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if 0 <= nx < n and 0 <= ny < n and sgraph[nx][ny] == c and svisited[nx][ny] == 0:
+                    q.append((nx, ny))
+    return 1
+
+n = int(input())
+graph = []
+visited = [[0 for i in range(n)] for i in range(n)]
+svisited = deepcopy(visited)
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+for i in range(n):
+    graph.append(list(input().rstrip()))
+
+sgraph = deepcopy(graph)
+for i in range(n):
+    for j in range(n):
+        if sgraph[i][j] == 'G':sgraph[i][j] = 'R'
+
+cnt = 0
+ccnt = 0
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 0:
+            cnt += bfs(i, j)
+
+for i in range(n):
+    for j in range(n):
+        if sgraph[i][j] != 0:
+            ccnt += sbfs(i, j)
+
+print(cnt, end=' ')
+print(ccnt)
